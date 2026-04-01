d2bf2d25c8646b4fffe42eaef7dd60f2f5106f31
Sudharsan D S <105963347+Sudharsan2618@users.noreply.github.com>
2026-04-01 17:32:38 +0530

    updated cross

--- run.py
+++ run.py
@@ -20,6 +20,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:5173",
         "https://tourmaline-malasada-9c1bc9.netlify.app",
+        "https://student-project-phi.vercel.app",
         os.getenv("FRONTEND_URL", "")
     ],
     allow_credentials=True,
